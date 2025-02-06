c046a5a2399c470f95510806a4f6fe2379572a20
Jude Tejada <judelaurentetejada@gmail.com>
2025-02-06 10:07:57 +0800

    chore

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,8 +2,8 @@ import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
   baseURL:
     process.env.NODE_ENV === 'development'
-      ? new URL('http://localhost:3000')
-      : new URL(`https://${process.env.VERCEL_URL!}`)
+      ? 'http://localhost:3000'
+      : `https://${process.env.VERCEL_URL!}`
 });
 
 export const { useSession } = createAuthClient();
