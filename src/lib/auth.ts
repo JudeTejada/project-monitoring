9fe0e4b867424e2124f076ae02288d1245d8d450
Jude Tejada <judelaurentetejada@gmail.com>
2025-02-06 10:56:40 +0800

    chore

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import prisma from './prisma';
 
 export const auth = betterAuth({
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL,
   database: prismaAdapter(prisma, {
     provider: 'postgresql'
   }),
