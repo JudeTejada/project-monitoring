c059531dc772e59303b05b0cb4dc9f471cd8eeca
Jude Tejada <judelaurentetejada@gmail.com>
2025-02-06 10:46:11 +0800

    :test

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,13 +5,13 @@ import { Navbar } from '../../components/common/Navbar';
 import { ProjectCards } from './components/ProjectCards';
 
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  });
+  // const session = await auth.api.getSession({
+  //   headers: await headers()
+  // });
 
-  if (!session) {
-    return redirect('/sign-in');
-  }
+  // if (!session) {
+  //   return redirect('/sign-in');
+  // }
   return (
     <div className='mt-10 container mx-auto w-full'>
       <Navbar />
